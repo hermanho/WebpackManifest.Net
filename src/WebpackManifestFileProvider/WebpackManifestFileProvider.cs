be34f9140f18439268c235f75f6d5a86882f4134
herman <hmcherman@gmail.com>
2017-02-05 01:48:21 +0800

    Fixing Manifest File change cannot be watched

--- src/WebpackManifestFileProvider/WebpackManifestFileProvider.cs
+++ src/WebpackManifestFileProvider/WebpackManifestFileProvider.cs
@@ -17,18 +17,21 @@ namespace WebpackManifestFileProvider
         private const string PollingEnvironmentKey = "DOTNET_USE_POLLING_FILE_WATCHER";
 
         private readonly PhysicalFileProvider _physicalFileProvider;
-        private readonly string _webpackManifestFile;
+        private readonly string _webpackManifestFileName;
+        private readonly string _webpackManifestFileFullPath;
         private readonly PhysicalFilesWatcher _webpackManifestFileWatcher;
         private Dictionary<string, string> _webpackManifestMapping = new Dictionary<string, string>();
         private ILogger<WebpackManifestFileProvider> _logger;
+        private IChangeToken _token;
 
         public WebpackManifestFileProvider(string root, string webpackManifestFile, ILoggerFactory loggerFactory)
         {
+            root = EnsureTrailingSlash(root);
             _physicalFileProvider = new PhysicalFileProvider(root);
-            _webpackManifestFile = Path.Combine(_physicalFileProvider.Root, webpackManifestFile);
+            _webpackManifestFileName = webpackManifestFile;
+            _webpackManifestFileFullPath = Path.Combine(_physicalFileProvider.Root, webpackManifestFile);
             _logger = loggerFactory.CreateLogger<WebpackManifestFileProvider>();
             _webpackManifestFileWatcher = CreateManifestFileWatcher(root);
-            CreateWatchCallback();
             LoadMapping();
         }
 
@@ -36,6 +39,17 @@ namespace WebpackManifestFileProvider
         {
             _webpackManifestFileWatcher.Dispose();
             _physicalFileProvider.Dispose();
+        }
+
+        private static string EnsureTrailingSlash(string path)
+        {
+            if (!string.IsNullOrEmpty(path) &&
+                path[path.Length - 1] != Path.DirectorySeparatorChar)
+            {
+                return path + Path.DirectorySeparatorChar;
+            }
+
+            return path;
         }
 
         private static PhysicalFilesWatcher CreateManifestFileWatcher(string root)
@@ -51,8 +65,9 @@ namespace WebpackManifestFileProvider
         {
             if (_webpackManifestMapping.ContainsKey(subpath))
             {
-                _logger.LogDebug($"Searching {subpath}");
+                string oldsubpath = subpath;
                 subpath = _webpackManifestMapping[subpath];
+                _logger.LogInformation($"Path {oldsubpath} change to {subpath}");
             }
             return _physicalFileProvider.GetFileInfo(subpath);
         }
@@ -69,39 +84,59 @@ namespace WebpackManifestFileProvider
 
         private void CreateWatchCallback()
         {
-            _logger.LogDebug($"Create Watch Callback");
-            var token = _webpackManifestFileWatcher.CreateFileChangeToken(_webpackManifestFile);
-            token.RegisterChangeCallback(_ => LoadMapping(), null);
+            _logger.LogInformation($"Create Watch Callback");
+            if (_token != null)
+            {
+
+            }
+            _token = _webpackManifestFileWatcher.CreateFileChangeToken(_webpackManifestFileName);
+            _token.RegisterChangeCallback(_ =>
+            {
+                _logger.LogInformation($"JSON file changed {_webpackManifestFileFullPath}");
+                LoadMapping();
+            }, null);
         }
 
         private void LoadMapping()
         {
-            if (File.Exists(_webpackManifestFile))
+            if (File.Exists(_webpackManifestFileFullPath))
             {
                 try
                 {
-                    _logger.LogInformation($"Loading JSON {_webpackManifestFile}");
-                    using (JsonTextReader reader = new JsonTextReader(File.OpenText(_webpackManifestFile)))
+                    System.Threading.Thread.Sleep(200);
+                    _logger.LogInformation($"Loading JSON {_webpackManifestFileFullPath}");
+                    using (var fs = new FileStream(_webpackManifestFileFullPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
                     {
-                        var newMap = new Dictionary<string, string>();
-                        JObject jsonObject = (JObject)JToken.ReadFrom(reader);
-                        _logger.LogDebug($"JSON read completed");
-                        foreach (KeyValuePair<string, JToken> node in jsonObject)
+                        using (var sr = new StreamReader(fs))
                         {
-                            newMap.Add('/' + node.Key, '/' + node.Value.Value<string>());
+                            using (JsonTextReader reader = new JsonTextReader(sr))
+                            {
+                                var newMap = new Dictionary<string, string>();
+                                JObject jsonObject = (JObject)JToken.ReadFrom(reader);
+                                _logger.LogDebug($"JSON read completed");
+                                foreach (KeyValuePair<string, JToken> node in jsonObject)
+                                {
+                                    newMap.Add('/' + node.Key, '/' + node.Value.Value<string>());
+                                }
+                                _webpackManifestMapping = newMap;
+                                _logger.LogInformation($"Loading JSON completed");
+                            }
                         }
-                        _webpackManifestMapping = newMap;
                     }
-                    _logger.LogInformation($"Loading JSON completed");
                 }
                 catch (JsonReaderException ex)
                 {
                     _logger.LogError(-1, ex, ex.Message);
                 }
+                catch (Exception ex)
+                {
+                    _logger.LogError(-1, ex, ex.Message);
+                    throw;
+                }
             }
             else
             {
-                _logger.LogDebug($"File does not exist {_webpackManifestFile}");
+                _logger.LogDebug($"File does not exist {_webpackManifestFileFullPath}");
             }
             CreateWatchCallback();
         }
